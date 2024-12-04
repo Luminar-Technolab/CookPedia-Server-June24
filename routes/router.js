4aafb1f07665dcac40f795057c7931a3543c4faa
soniagkalliath <soniageorge111@gmail.com>
2024-12-04 16:16:01 +0530

    testimony added

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const recipeController = require('../controllers/recipeController')
+const testimonyController = require("../controllers/testimonyController")
 
 const router = new express.Router()
 
 //all recipes
 router.get("/all-recipes",recipeController.getAllRecipeController)
+//add-testmony
+router.post("/add-testimony",testimonyController.addTestimonyController)
+
 
 module.exports = router
